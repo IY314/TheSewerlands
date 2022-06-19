52ae2be61fc0cca3f5b4e511f2460b239174d1f4
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-06-19 01:04:44 -0700

    Add individual sprite loading function

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -5,19 +5,26 @@ sf::Vector2i operator*(const sf::Vector2i & a, const sf::Vector2i & b)
     return {a.x * b.x, a.y * b.y};
 }
 
+sf::Texture loadTexture(const std::string & filename)
+{
+    sf::Texture texture;
+    if (!texture.loadFromFile(filename))
+    {
+        if (!texture.loadFromFile("data/unknown.png"))
+        {
+            throw std::runtime_error("Failed to load unknown.png");
+        }
+    }
+    return texture;
+}
+
 namespace swr
 {
     Spritesheet::Spritesheet(const std::string & filename,
                              const sf::Vector2i & size)
-        : m_size(size)
+        : m_size(size),
+          m_texture(loadTexture(filename))
     {
-        if (!m_texture.loadFromFile(filename))
-        {
-            if (!m_texture.loadFromFile("data/unknown.png"))
-            {
-                throw std::runtime_error("Failed to load unknown.png");
-            }
-        }
     }
 
     sf::Sprite Spritesheet::operator()(const sf::Vector2i & coords,
@@ -26,6 +33,17 @@ namespace swr
         sf::Sprite sprite;
         sprite.setTexture(m_texture);
         sprite.setTextureRect({coords * m_size, m_size * size});
+        return sprite;
+    }
+
+    sf::Sprite getSprite(const std::string & filename,
+                         const sf::Vector2i & coords,
+                         const sf::Vector2i & size)
+    {
+        const sf::Texture texture = loadTexture(filename);
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
+        sprite.setTextureRect({coords, size});
         return sprite;
     }
 }  // namespace swr
--- src/sprite.hpp
+++ src/sprite.hpp
@@ -20,4 +20,8 @@ namespace swr
         sf::Texture m_texture;
         sf::Vector2i m_size;
     };
+
+    sf::Sprite getSprite(const std::string & filename,
+                         const sf::Vector2i & coords,
+                         const sf::Vector2i & size = {16, 16});
 }  // namespace swr
