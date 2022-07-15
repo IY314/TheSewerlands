018859158576c48f4e0bd90b2af1ae58549b707d
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-07-15 10:45:58 -0700

    Add neighbors

--- src/room.cpp
+++ src/room.cpp
@@ -8,13 +8,11 @@ namespace swr
                const std::string &name,
                const std::string &bgpath,
                const util::Vec2<int> &pos,
-               const std::vector<Room> &neighbors,
                const util::Vec2<int> &size) noexcept
         : m_name(name),
           m_pos(pos),
           m_size(size),
-          m_bgtex(IMG_LoadTexture(rend, bgpath.c_str())),
-          m_neighbors(neighbors)
+          m_bgtex(IMG_LoadTexture(rend, bgpath.c_str()))
     {
     }
 
@@ -23,7 +21,7 @@ namespace swr
     void Room::update() noexcept
     {
         // TODO: update room
-        for (auto &room : m_neighbors) room.update();
+        for (auto &[d, room] : m_neighbors) room.update();
         for (auto &object : m_objects) object.update();
     }
 
@@ -32,7 +30,12 @@ namespace swr
         SDL_Rect clip{m_pos.x, m_pos.y, m_size.x * 5, m_size.y * 5};
         SDL_RenderCopy(rend, m_bgtex, NULL, &clip);
 
-        for (const auto &room : m_neighbors) room.render(rend);
+        for (const auto &[d, room] : m_neighbors) room.render(rend);
         for (const auto &object : m_objects) object.render(rend);
+    }
+
+    void Room::add_neighbor(Facing facing, Room &room) noexcept
+    {
+        m_neighbors.insert({facing, room});
     }
 }  // namespace swr
